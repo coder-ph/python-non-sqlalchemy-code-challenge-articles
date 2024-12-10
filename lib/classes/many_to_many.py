d829cec059bb40d1052b9f05c1a2974119fc8a6e
Phelix <phelixmbani@gmail.com>
2024-12-10 17:25:21 +0300

    updates top_publisher

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,7 +2,11 @@ class Author:
     def __init__(self, name):
         if not isinstance(name, str) or len(name) <= 0:
             raise ValueError("Name must be a non-empty string")
+        
+        if hasattr(self, '_name'):
+            raise AttributeError("Name should not change after the author is instantiated")
         self._name = name
+        
         self._articles = []
 
     @property
@@ -17,12 +21,20 @@ class Author:
         return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
+        
         if not isinstance(magazine, Magazine):
             raise ValueError("magazine must be an instance of Magazine")
+         
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string between 5 and 50 characters")
+    
         article = Article(self, magazine, title)
+        
         self._articles.append(article)
-        magazine.articles().append(article)
+          
+        magazine._articles.append(article)
         return article
+        
 
     def topic_areas(self):
         if not self._articles:
@@ -31,14 +43,19 @@ class Author:
 
 
 class Magazine:
+    all = []
     def __init__(self, name, category):
         if not isinstance(name, str) or not (2 <= len(name) <= 16):
             raise ValueError("Name must be a string with 2-16 characters")
+        self.name = name
+        
         if not isinstance(category, str) or len(category) <= 0:
             raise ValueError("Category must be a non-empty string")
-        self._name = name
-        self._category = category
+        self.category = category
+        
         self._articles = []
+        
+        Magazine.all.append(self)
 
     @property
     def name(self):
@@ -58,6 +75,7 @@ class Magazine:
     def category(self, value):
         if not isinstance(value, str) or len(value) <= 0:
             raise ValueError("Category must be a non-empty string")
+        
         self._category = value
 
     def articles(self):
@@ -81,24 +99,29 @@ class Magazine:
         
         contributing_authors = [author for author, count in author_count.items() if count > 2]
         
-        # If there are no contributing authors, return None instead of an empty list
         return contributing_authors if contributing_authors else None
+    
+    @classmethod
+    def top_publisher(cls):
+        if not cls.all:  
+            return None
+        
+        magazines_with_articles = [magazine for magazine in cls.all if magazine.articles()]
+        if not magazines_with_articles: 
+            return None
+        
+        top_magazine = max(magazines_with_articles, key=lambda magazine: len(magazine.articles()))
+        return top_magazine
 
 
 class Article:
     all = []
     def __init__(self, author, magazine, title):
-        if not isinstance(author, Author):
-            raise ValueError("Author must be an instance of Author")
-        if not isinstance(magazine, Magazine):
-            raise ValueError("Magazine must be an instance of Magazine")
-        
-        if not isinstance(title, str) or not (5 <= len(title) <= 50):
-            raise ValueError("Title must be a string with 5-50 characters")
-
-        self._author = author
-        self._magazine = magazine
+    
+        self.author = author
+        self.magazine = magazine
         self._title = title
+        
         author.articles().append(self)
         magazine.articles().append(self)
         Article.all.append(self)
@@ -130,5 +153,8 @@ class Article:
     @title.setter
     def title(self, title):
         if hasattr(self, '_title'):
+            raise AttributeError("Title cannot be changed after instantiation")
+        
+        if hasattr(self, '_title') or not (5 <= len(title) <= 50):
             raise AttributeError ('cannot be changed after instantiated')
         self._title = title  
